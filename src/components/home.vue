328030fab00606391b5dcd740fd4789ec94dea3e
Infinitx <37289102+Infinitx@users.noreply.github.com>
2018-04-18 20:55:45 +0800

    Add files via upload

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,17 @@
 <template>
 	<div>
 			<ul>
-				<router-link tag="li" to="/home/today" >今日10点</router-link>
-				<router-link tag="li" to="/home/tomorrow" >明日10点</router-link>
+				<router-link tag="li" to="/home/today" activeClass="active">今日10点上新</router-link>
+				<router-link tag="li" to="/home/tomorrow" activeClass="active">明日10点上新</router-link>
 			</ul>	
-
+			
 			<router-view></router-view>
 	</div>
 </template>
 
-<script type="text/javascript"> 
+<script type="text/javascript">
+//import observer  from "../observer"
+
 	export default {
 
 		data(){
@@ -18,12 +20,29 @@
 			}
 		},
 
-		
+		/*mounted(){
+			// observer.publish("default");
+			this.$store.commit("changeMaizuoTitle","卖座电影");
+		}*/
 	}
 </script>
 
 
-<style type="text/css" scoped>
+<style type="text/css" lang="scss" scoped>
+	ul{
+		display: flex;
+		li{
+			font-size: 0.16rem;
+			list-style: none;
+			flex:1;
+			height: 0.4rem;
+			line-height: 0.4rem;
+			text-align: center;
+		}
+	}
 
+	.active{
+		color:#fe6e00;
+	}
 
 </style>

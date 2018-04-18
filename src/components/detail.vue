e8b969358370d41c4f09178194bd6f4ea062b8be
Infinitx <37289102+Infinitx@users.noreply.github.com>
2018-04-18 20:54:50 +0800

    Add files via upload

---
+++ src/components/detail.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		detail
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+
+			}
+		}
+	}
+</script>
+<style type="text/css" lang="scss" scoped>
+	
+</style>

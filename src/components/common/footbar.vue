64052fada8bf63bbd70e5416841787e4fe9cd118
luoxiazx <1958745740@qq.com>
2018-04-19 17:28:40 +0800

    updatacommon

--- src/components/common/footbar.vue
+++ src/components/common/footbar.vue
@@ -1,5 +1,26 @@
 <template>
-	
+	<footer>
+		<ul @click="handleClick()">
+			<router-link to="/home" tag="li">
+				<i class="iconfont icon-store"></i>
+				<p>首页</p>
+			</router-link>
+			<router-link to="/group" tag="li">
+				<i class="iconfont icon-smile"></i>
+				<p>拼团</p>
+			</router-link>
+			<router-link to="/car" tag="li">
+				<i class="iconfont icon-bags"></i>
+				<p>购物车</p>
+			</router-link>
+			<slot></slot>
+			<router-link to="/my" tag="li" @click="show=false">
+				<i class="iconfont icon-account"></i>
+				<p>我的</p>
+			</router-link>
+
+		</ul>
+	</footer>
 </template>
 
 <script type="text/javascript">
@@ -7,13 +28,47 @@
 
 		data(){
 			return {
-				
+
+			}
+		},
+		methods:{
+			handleClick(){
+				this.$emit("event");
 			}
 		}
 	}
 </script>
 
 
-<style>
-	
+<style type="text/css" lang="scss" scoped>
+li{
+	list-style: none;
+}
+footer{
+	background: #fff;
+	font-size: 0.14rem;
+	position: fixed;
+	z-index: 1;
+	bottom: 0;
+	height: 0.5rem;
+	width: 100%;
+	border-top: 1px solid rgb(238, 238, 238);
+	ul{
+		height: 0.5rem;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		li{
+			text-align: center;
+			a{
+				text-decoration: none;
+				color: #666;
+			}
+		}
+		.router-link-exact-active{
+			color: #fe4070;
+		}
+	}
+}
+
 </style>
--- src/components/common/sidebar.vue
+++ src/components/common/sidebar.vue
@@ -1,19 +1,123 @@
 <template>
-	
+	<transition name="bounce">
+	<aside>
+		<div class="side">
+		<header>
+			<a class="search">
+				<img src="//f0.jmstatic.com/btstatic/h5/common/search_btn.png" style="display: inline-block;">
+				<span>搜索商品 分类 功效</span>
+			</a>
+			<slot></slot>
+		</header>
+		<ul @click="handleClick()">
+			<li v-for="(data,index) in datalist">
+				{{data.name}}
+				<i class="iconfont icon-moreunfold"></i>
+			</li>
+		</ul>
+	</div>
+	</aside>
+	</transition>
 </template>
 
 <script type="text/javascript">
+import axios from "axios";
 	export default {
 
 		data(){
 			return {
-				
+				datalist:[],
+				datalist1:[]
 			}
-		}
+		},
+		methods:{
+			handleClick(){
+				//console.log();
+			}
+		},
+		mounted(){
+		 axios.get("/index/requestDelegate?url=http%3A%2F%2Fmobile.jumei.com%2Fmsapi%2Fmall%2Fallcategories.json"
+	 ).then(res=>{
+		 			//console.log(res.data.data);
+					//console.log(res.data.data[0].sub_categories);
+					this.datalist=res.data.data;
+					this.datalist1=res.data.data[0].sub_categories
+		 		})
+		 }
 	}
 </script>
 
 
-<style>
-	
+<style type="text/css" lang="scss" scoped>
+	li{
+		list-style: none;
+	}
+	aside{
+		font-size: 0.14rem;
+		width: 80%;
+		height: 100%;
+		position: fixed;
+		z-index: 5;
+		top: 0;
+		right: 0;
+		background: #fff;
+		border: 1px solid #f5f5f5;
+		header{
+			padding: 0 0.1rem;
+			height: 0.45rem;
+			display: flex;
+			align-items: center;
+			width: 100%;
+			box-sizing: border-box;
+			border-bottom: 1px solid #f5f5f5;
+			.search{
+				margin: 0.05rem 0.2rem 0.05rem 0.1rem;
+		    color: #999;
+		    background: #f5f5f5;
+		    border-radius: 0.15rem;
+		    height: 0.25rem;
+		    line-height: 0.25rem;
+		    -webkit-box-shadow: 0 0.0625rem 0.125rem rgba(0,0,0,.05) inset;
+		    box-shadow: inset 0 0.0625rem 0.125rem rgba(0,0,0,.05);
+		    font-size: 0.12rem;
+		    text-align: center;
+				flex: 1;
+				img{
+					width: 0.14rem;
+				}
+			}
+			.right{
+				height: 0.18rem;
+				img{
+					width: 0.24rem;
+				}
+			}
+		}
+		ul{
+			overflow: auto;
+			li{
+				height: 0.4rem;
+				display: flex;
+				padding: 0 0.1rem;
+				justify-content: space-between;
+				align-items: center;
+				border-bottom: 1px solid #f5f5f5;
+			}
+		}
+	}
+
+	.bounce-enter-active {
+		animation: bounce-in .5s;
+	}
+	.bounce-leave-active {
+		animation: bounce-in .5s reverse;
+	}
+	@keyframes bounce-in {
+		0% {
+			transform:translateX(100%);
+		}
+		100% {
+			transform:translateX(0px);
+		}
+	}
 </style>

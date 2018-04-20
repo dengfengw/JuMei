743656347d021a1f676f9f4d27e22213aecc477e
Infinitx <37289102+Infinitx@users.noreply.github.com>
2018-04-20 20:54:32 +0800

    获取动态数据，实现页面跳转
    Home  click to jump to detail e, and get dynamic data.

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,18 +1,50 @@
 <template>
 
-	<div id="box" v-if="info"> 
-		<img src="http://p0.jmstatic.com/banner/area/000/000/029.jpg" id="right">
-		<swipe class="my-swipe">
-			  <swipe-item v-for="data in looplist" :key="data.__ob__.id">
-			  		<img :src="data['800']"/>
-			  </swipe-item>
-			</swipe>	
-		<p id="price"><b></b><span></span><a>价格详情</a></p>
-		<p id="nothing">已抢光</p>
-		<div id="title"><b>{{info.data.special_tags.jm_owner}}</b><span>{{info.data.name}}</span></div>
-		<div id="freight"><span>运费</span><a>本商品满299或2件包邮（新疆部分区域除外）</a></div>
-		<div id="explain"><span>说明</span><a>自营保税仓发货&nbsp;&nbsp;24小时内发货</br>7天拆封无条件退货&nbsp;&nbsp;支持分期</a></div>
+	<div id="box" style="position: absolute;top:0;z-index: 4;"> 
+		<div style="
+    height: 0.5rem;
+    background: #fff;
+    font-size: 0.14rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0.3rem;
+
+">
+			<router-link class="iconfont icon-back" style="font-size: 0.25rem;" to="/home/today"></router-link>
+		 	<div v-if="info">{{info.data.properties[0].value}}</div> 
+		 	<i class="iconfont icon-all" style="font-size: 0.25rem;"></i>
+		</div>
+		<div v-if="info">
+			<img :src="info.data.area_icon_v2" id="right">
+			<swipe class="my-swipe">
+				<swipe-item v-for="data in looplist" :key="data.__ob__.id">
+				  	<img :src="data['800']"/>
+				</swipe-item>
+			</swipe>
+		</div>
+		
+		<div v-if="info1">
+			<p id="price"><b>￥{{info1.data.result.jumei_price}}</b><span>￥{{info1.data.result.market_price}}</span><a>价格详情</a><i>{{info1.data.result.product_desc}}</i></p>
+			<p id="nothing">已抢光</p>
+		</div>
+		<div v-if="info">
+			<div id="title"><b>{{info.data.special_tags.jm_owner}}</b><span>{{info.data.name}}</span></div>
+		</div>
+		<div v-if="info1">
+			
+			<div id="freight"><span>运费</span><a>{{info1.data.result.freight.text}}</a></div>
+		<div id="explain"><span>说明</span><a>{{info1.data.result.icon_tag[0].name}}&nbsp;&nbsp;{{info1.data.result.icon_tag[1].name}}</br>{{info1.data.result.icon_tag[2].name}}&nbsp;&nbsp;支持分期</a></div>
 		<div id="choose"><span>请选择</span><a>型号&nbsp;&nbsp;分类</a></div>
+		</div>
+		<div id="shop" v-if="info1">
+				<img :src="info1.data.result.shop_info.logo_url['320']">
+				<p id="shop_title">{{info1.data.result.shop_info.store_title}}</p><a>{{info1.data.result.shop_info.is_proprietary}}</a><b>{{info1.data.result.shop_info.is_authorization}}</b>
+				<i>{{info1.data.result.shop_info.button_text}}</i>
+			</div>	
+		<div id="box_car">
+			<span class="iconfont icon-store">店铺</span><span class="iconfont icon-cart">购物车</span><span>加入购物车</span><span>立即购买</span>
+		</div>
 	</div>
 </template>
 
@@ -21,12 +53,12 @@ import axios from "axios";
 //import observer from "../observer"
 import 'vue-swipe/dist/vue-swipe.css';
 
-// in ES6 modules
 import { Swipe, SwipeItem } from 'vue-swipe';
 	export default{
 		data(){
 			return{
 				info:null,
+				info1:null,
 				looplist:[]
 			}
 		},
@@ -36,11 +68,20 @@ import { Swipe, SwipeItem } from 'vue-swipe';
 		},
 		mounted(){
 			console.log(this.$route.params.id);
-			axios.get(`/product/ajaxStaticDetail?item_id=ht171007p1441587t1&type=global_deal`).then(res=>{
+			axios.get("/product/ajaxStaticDetail?item_id="+this.$route.params.id+"&type=global_deal").then(res=>{
 				console.log(res.data)
 				this.info = res.data
+				console.log(this.info.data.area_icon_v2);
 				this.looplist = res.data.data.image_url_set.single_many
 				console.log(this.looplist);
+			})
+			axios.get("/product/ajaxDynamicDetail?item_id="+this.$route.params.id+"&type=global_deal").then(res=>{
+				console.log(res.data)
+				this.info1 = res.data
+				//console.log(this.info.data.area_icon_v2);
+				console.log(this.info1.data.result.shop_info.logo_url['320']);
+				//this.looplist = res.data.data.image_url_set.single_many
+				//console.log(this.looplist);
 			})
 			
 		}
@@ -52,6 +93,9 @@ import { Swipe, SwipeItem } from 'vue-swipe';
 		//
 		overflow: hidden;
 	}
+	img{
+		display: block;
+	}
 	.my-swipe {
 	  height: 3.75rem;;
 
@@ -60,9 +104,10 @@ import { Swipe, SwipeItem } from 'vue-swipe';
 	  }
 	}
 	#right{
-		margin-left: -0.1rem;
+		margin-left: -0.03rem;
 		width: 15%;
 		position: absolute;
+		z-index: 3;
 	}
 	#price{
 		margin-left: 0.1rem;
@@ -73,7 +118,6 @@ import { Swipe, SwipeItem } from 'vue-swipe';
 		}
 		b{
 			font-weight: 100;
-			display: block;
 			color: red;
 			font-size: 0.22rem;
 		}
@@ -91,6 +135,13 @@ import { Swipe, SwipeItem } from 'vue-swipe';
 			margin-left: 0.06rem;
 			font-weight: 100;
 			color: #666;
+		}
+		i{
+			margin-top: 0.08rem;
+			font-style: normal;
+			display: block;
+			float:right;
+			margin-right: 0.2rem;
 		}
 	}
 	#nothing{
@@ -153,5 +204,49 @@ import { Swipe, SwipeItem } from 'vue-swipe';
 			color: #999;
 			margin-right: 0.30rem;
 		}
+	}
+	#shop{
+		height: 1.6rem;
+		overflow: hidden;
+		img{
+			float: left;
+			border: 0.01rem solid #999;
+			width: 12%;
+			margin: 0.1rem 0.1rem;
+		}
+		#shop_title{
+			float: left;
+			margin-top: 0.12rem;
+			font-size: 0.15rem;
+		}
+		a,b{
+			display: block;
+			float: left;
+			margin-left: 0.04rem;
+			margin-top: 0.15rem;
+			background-color: #fe4070;
+			color: white;
+			padding:0 0.02rem;
+			border-radius: 0.04rem;	
+			}
+		i{
+			width: 1rem;
+			display: block;
+			margin-top: 0.6rem;
+			margin-left: 1.2rem;
+			font-style: normal;
+			font-size: 0.16rem;
+			padding: 0.08rem 0.2rem;
+			text-align: center;
+			border: 0.01rem solid #999;
+			border-radius: 0.4rem;
+		}
+	}
+	#box_car{
+		margin: 0.4rem 0;
+		background: white;
+		z-index: 4;
+		position: fixed;
+		bottom: 0;
 	}
 </style>
